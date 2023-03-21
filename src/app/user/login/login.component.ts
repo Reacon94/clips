1852610b83be842580730116ddeac3fde1611a2f
max <max.neuwirt@gmx.de>
2023-03-21 17:35:28 +0100

    persistancesession

--- src/app/user/login/login.component.ts
+++ src/app/user/login/login.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { AngularFireAuth,  } from '@angular/fire/compat/auth';
+import { getAuth, setPersistence, signInWithEmailAndPassword, browserSessionPersistence } from "firebase/auth";
+
+
+
+
 
 @Component({
   selector: 'app-login',
@@ -15,6 +20,7 @@ export class LoginComponent implements OnInit {
   alertMsg = "Please wait! We are logging you in."
   alertColor = "blue"
   inSubmission = false
+  hi = getAuth()
 
   constructor(private auth: AngularFireAuth) { }
 
@@ -23,8 +29,27 @@ export class LoginComponent implements OnInit {
     this.alertMsg = "Please wait! We are logging you in."
     this.alertColor = "blue"
     this.inSubmission = true
+    
+
+    setPersistence(this.hi, browserSessionPersistence)
+  .then(() => {
+    // Existing and future Auth states are now persisted in the current
+    // session only. Closing the window would clear any existing state even
+    // if a user forgets to sign out.
+    // ...
+    // New sign-in will be persisted with session persistence.
+    return signInWithEmailAndPassword(this.hi, this.credentials.email, this.credentials.password);
+  })
+  .catch((error) => {
+    // Handle Errors here.
+    const errorCode = error.code;
+    const errorMessage = error.message;
+  });
+    
+/* Old Version
 
     try{
+    
       await this.auth.signInWithEmailAndPassword(
         this.credentials.email, this.credentials.password
       )
@@ -37,6 +62,10 @@ export class LoginComponent implements OnInit {
 
       return
     }
+    this.alertMsg = "Success! You are now logged in."
+    this.alertColor = "green"
+
+    */
     this.alertMsg = "Success! You are now logged in."
     this.alertColor = "green"
   }
